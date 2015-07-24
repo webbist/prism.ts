e2f7ca2dbe8f6905de664e1e23380e42cd6c572e
spasas <spasas@hotmail.com>
2015-07-23 20:00:53 -0400

    Extraction of the typescript embedded in Tennis en Ligne Cordova app to start the refactoring of the API.

---
+++ prism.js
@@ -0,0 +1,46 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+define(["require", "exports", "./mono"], function(require, exports, System) {
+    (function (PubSubEvents) {
+        var EventAggregator = (function () {
+            function EventAggregator() {
+            }
+            EventAggregator.GetEvent = function (event) {
+                return new event();
+            };
+            return EventAggregator;
+        })();
+        PubSubEvents.EventAggregator = EventAggregator;
+
+        var EventBase = (function () {
+            function EventBase() {
+            }
+            return EventBase;
+        })();
+        PubSubEvents.EventBase = EventBase;
+
+        var PubSubEvent = (function (_super) {
+            __extends(PubSubEvent, _super);
+            function PubSubEvent() {
+                _super.apply(this, arguments);
+            }
+            PubSubEvent.prototype.Publish = function (eventArgs) {
+                $.publish(this.constructor.name, eventArgs);
+            };
+
+            PubSubEvent.prototype.Subscribe = function (instance, action) {
+                $.subscribe(this.constructor.name, function (event, eventArgs) {
+                    action.apply(instance, [eventArgs]);
+                });
+            };
+            return PubSubEvent;
+        })(EventBase);
+        PubSubEvents.PubSubEvent = PubSubEvent;
+    })(exports.PubSubEvents || (exports.PubSubEvents = {}));
+    var PubSubEvents = exports.PubSubEvents;
+});
+//# sourceMappingURL=prism.js.map
---
+++ prism.ts
@@ -0,0 +1,39 @@
+﻿// copyright 2015 Webbist Multimedia Enr.
+
+// licensed under the Apache License, Version 2.0(the "License");
+// you may not use this file except in compliance with the License.
+// you may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+// either express or implied.
+// see the License for the specific language governing permissions and limitations under the License.
+
+import System = require("./mono");
+
+import EventArgs = System.EventArgs;
+
+export module PubSubEvents {
+    export class EventAggregator {
+        static GetEvent<TEventType extends EventBase>(event: { new (): TEventType; }): TEventType {
+            return new event();
+        }
+    }
+
+    export class EventBase {
+    }
+
+    export class PubSubEvent<TPayload extends EventArgs> extends EventBase {
+        Publish(eventArgs: TPayload): void {
+            $.publish((<any>this.constructor).name, eventArgs);
+        }
+
+        Subscribe(instance: any, action: (eventArgs: TPayload) => void): void {
+            $.subscribe((<any>this.constructor).name, function (event, eventArgs) {
+                action.apply(instance, [eventArgs]);
+            });
+        }
+    }
+}
